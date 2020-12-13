f6ea0afdd4acb3ee615b4186b242090fb317f14b
Haley Wright <haley.ren.wright@gmail.com>
2020-12-13 14:01:19 -0800

    part 2 of day 9

--- d9/index.js
+++ d9/index.js
@@ -17,6 +17,20 @@ function hasSum(num, arr) {
   return false;
 }
 
+function getSumList(num, arr) {
+  let a, sum;
+  for (let i = 0; i < arr.length; i++) {
+    a = [];
+    for (let j = i; j < arr.length; j++) {
+      a.push(arr[j]);
+      sum = a.reduce((s, n) => s + n, 0);
+      if (num == sum) return a.sort();
+      if (sum > num) break;
+    }
+  }
+  return [];
+}
+
 fs.readFile('./d9/input.txt', 'utf-8', (err, str) => {
   if (err) {
     console.error(err);
@@ -27,11 +41,18 @@ fs.readFile('./d9/input.txt', 'utf-8', (err, str) => {
   let prevNums = 25;
   let index = 25;
 
-  let valid = true;
+  let valid = true, invalidNum, list;
   for (let i = index; i < data.length; i++) {
     valid = hasSum(data[i], data.slice(i - prevNums, i));
     if (!valid) {
-      console.log(data[i]);
+      invalidNum = data[i];
+      console.log(invalidNum);
+
+      // find contiguous set of at least 2 numbers
+      list = getSumList(invalidNum, data.slice(0, i));
+      let min = list[0];
+      let max = list[list.length - 1];
+      console.log(min, max, min + max);
       return;
     }
   }
